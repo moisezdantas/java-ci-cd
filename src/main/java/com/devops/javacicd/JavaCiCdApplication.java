1187723f37a31937d8a043237a89c15b6c546133
Moisez Dantas <moisez.dantas@visto.global>
2021-04-02 01:47:08 -0300

    commmit inital

---
+++ src/main/java/com/devops/javacicd/JavaCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.devops.javacicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/javacicd/JavaCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.javacicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
