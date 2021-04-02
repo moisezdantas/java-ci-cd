f06b1bbfd646d0fd42216da272bea9e1f3fc439c
Moisez Dantas <moisez.dantas@visto.global>
2021-04-02 02:34:26 -0300

    add class pessoa

---
+++ src/main/java/com/devops/javacicd/dto/PessoaDTO.java
@@ -0,0 +1,16 @@
+package com.devops.javacicd.dto;
+
+public class PessoaDTO {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
